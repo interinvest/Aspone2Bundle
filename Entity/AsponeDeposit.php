0fe66a2ca0bd304561d21a608f7d98529e2a0d9c
MBrice76 <bmari@inter-invest.fr>
2017-09-19 15:16:41 +0200

    Update AsponeDeposit.php

--- Entity/AsponeDeposit.php
+++ Entity/AsponeDeposit.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace interinvest\aspone2bundle\Entity;
+namespace InterInvest\Aspone2Bundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
