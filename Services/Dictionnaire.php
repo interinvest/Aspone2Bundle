5bc7d3d0a8e6fdcaf4789ff9c80c6be7e371748e
Brice MARI <bmari@imac-de-brice-2.home>
2017-10-03 13:50:59 +0200

    commit

--- Services/Dictionnaire.php
+++ Services/Dictionnaire.php
@@ -19,7 +19,7 @@ class Dictionnaire
     private $groupe;
     private $annee;
     private $formulaire;
-    public static $data;
+    public static $data = [];
 
 
     public function __construct(Kernel $kernel)
@@ -38,11 +38,18 @@ class Dictionnaire
 
     public function parseFichier()
     {
-        foreach (glob($this->kernel->locateResource('@Aspone2Bundle/Resources/dictionnaires/').'*'.$this->groupe.'*.csv') as $filename) {
+        if(!$file = glob($this->kernel->locateResource('@Aspone2Bundle/Resources/dictionnaires/').'*'.$this->groupe.'*'.$this->annee.'*.csv')){
+            $file =  glob($this->kernel->locateResource('@Aspone2Bundle/Resources/dictionnaires/').'*'.$this->groupe.'*.csv');
+            $this->annee = \substr($file[0], -6, 2);
+        }
+
+        foreach ( $file as $filename) {
             if (($handle = fopen($filename, "r")) !== FALSE) {
                 while (($line = fgetcsv($handle, 1000, ",")) !== FALSE) {
                     $line = explode(';', $line[0]);
-                    self::$data[$line[0]][$line[1]][$line[2]][] = $line[7];
+                    if((in_array($line[0], $this->formulaire) || stristr($line[0], "IDENTIF")) && $line[1] == $this->annee) {
+                        self::$data[$line[0]][$line[2]][] = $line[7];
+                    }
                 }
                 fclose($handle);
             }
@@ -54,20 +61,6 @@ class Dictionnaire
      */
     public function getZones()
     {
-        $annee = $this->annee;
-        $zones = [];
-        foreach($this->formulaire as $formulaire) {
-            if(isset(self::$data[$formulaire][$annee])) {
-                $zones[$formulaire] = self::$data[$formulaire][$annee];
-            }
-            if (isset(self::$data["T-IDENTIF"][$annee])) {
-                $zones["T-IDENTIF"] = self::$data["T-IDENTIF"][$annee];
-            }
-            if (isset(self::$data["F-IDENTIF"][$annee])) {
-                $zones["F-IDENTIF"] = self::$data["F-IDENTIF"][$annee];
-            }
-        }
-
-        return $zones;
+        return self::$data;
     }
 }
