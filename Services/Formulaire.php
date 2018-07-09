346a39836a021a7436b8502d380cc432c1c5b221
MBrice76 <bmari@inter-invest.fr>
2018-07-09 15:57:59 +0200

    Update Formulaire.php

--- Services/Formulaire.php
+++ Services/Formulaire.php
@@ -289,6 +289,8 @@ class Formulaire
         try {
             $doc->schemaValidate($this->kernel->locateResource('@Aspone2Bundle/Resources/xsd/XmlEdi' . ucfirst(strtolower($this->declarable->getGroupe())) . '.xsd'));
         } catch (\Exception $E) {
+            dump($this->sDictionnaire->getZones());
+            dump($doc->savexml());
             throw new \Exception ('Erreur lors de la validation du XML : '. $E->getMessage(), 0);
         }
 
