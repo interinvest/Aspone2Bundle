3c56b220038af8c1934d8d565288e049b0dc2286
MBrice76 <bmari@inter-invest.fr>
2019-10-01 15:12:42 +0200

    Update AsponeDepositRepository.php

--- Repository/AsponeDepositRepository.php
+++ Repository/AsponeDepositRepository.php
@@ -19,10 +19,14 @@ class AsponeDepositRepository extends EntityRepository
 
     public function getDepositsEnvoyes()
     {
+        $dateMin = new DateTime();
+        $dateMin->modify('-3 hours');
+
         $query = $this->createQueryBuilder('depo')
             ->where('depo.etat = :etat')->setParameter('etat', AsponeDeposit::ETAT_NON_FINI)
             ->andWhere('depo.identifiant is not null')
-            ->andWhere('depo.retourImmediat = 1');
+            ->andWhere('depo.retourImmediat = 1')
+            ->andWhere('depo.dateEnvoi <= :dateMin')->setParameter('dateMin', $dateMin);
 
         return $query->getQuery()->iterate();
     }
